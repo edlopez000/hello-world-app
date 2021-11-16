0484308a06a8fcf9744c16d529067618c366dbfe
edlopez000 <edlopez000>
2021-11-16 12:14:03 -0500

    Added routing to App.js

---
+++ src/components/About.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function About() {
+  return (
+    <div>
+      <h1>About Page</h1>
+      <p>What can this page have?</p>
+      <p>Testing this whole shebang out</p>
+    </div>
+  );
+}

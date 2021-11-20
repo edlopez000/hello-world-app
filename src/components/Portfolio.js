cb5b015d96bbec82f95b1173e5ebb613be503ad3
edlopez000 <lopez.edward@outlook.com>
2021-11-20 17:42:03 -0500

    Rearranged file structure and began loading portions of the projects.json file

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,11 +1,21 @@
-import { Box, Center, Heading } from '@chakra-ui/layout';
+import { Box, Center, Heading, List, ListItem, Text } from '@chakra-ui/layout';
 import React from 'react';
+import json from '../static/projects.json';
 
 export default function Portfolio() {
   return (
     <Box>
       <Center>
         <Heading>Portfolio</Heading>
+      </Center>
+      <Center>
+        <List>
+          <ListItem>
+            {json.map((item) => {
+              return <Text>{item.projectTitle}</Text>;
+            })}
+          </ListItem>
+        </List>
       </Center>
     </Box>
   );

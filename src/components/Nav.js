8c13a894c4fefb081f6e7cad0ea4985770299925
edlopez000 <lopez.edward@outlook.com>
2021-11-21 18:37:52 -0500

    Added css and changed Nav.js

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,15 +1,27 @@
-import { Box, Divider, Stack } from '@chakra-ui/layout';
+import { Box, Divider, Stack, Text } from '@chakra-ui/react';
 import React from 'react';
 import { Link } from 'react-router-dom';
+import '../components/Nav.css';
 
 export default function Nav() {
   return (
     <Box className="nav-bar" mx="5">
       <Stack direction="row" justify="flex-end">
-        <Link to="/">Home</Link>
-        <Link to="/about">About</Link>
-        <Link to="/portfolio">Portfolio</Link>
-        <Link to="/contact">Contact</Link>
+        <Link className="link" to="/">
+          Home
+        </Link>
+        <Text className="link">|</Text>
+        <Link className="link" to="/about">
+          About
+        </Link>
+        <Text className="link">|</Text>
+        <Link className="link" to="/portfolio">
+          Portfolio
+        </Link>
+        <Text className="link">|</Text>
+        <Link className="link" to="/contact">
+          Contact
+        </Link>
       </Stack>
       <Divider />
     </Box>

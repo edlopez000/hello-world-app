fbdab8c75406c79f7339bd2da945f0ff2d1ec71b
edlopez000 <lopez.edward@outlook.com>
2021-12-04 23:04:25 -0500

    Refactored joke refresh mechanism in Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -12,28 +12,30 @@ import React, { useEffect, useState } from 'react';
 
 export default function Home() {
   const [dadJoke, setDadJoke] = useState([]);
-  const [showJoke, toggleShowJoke] = useState(false);
+  const [showJoke, setShowJoke] = useState(false);
 
   const handleJoke = () => {
-    toggleShowJoke(!showJoke);
+    setShowJoke(!showJoke);
   };
 
-  const fetchJoke = () => {
-    return fetch('https://icanhazdadjoke.com/', {
-      headers: {
-        Accept: 'application/json',
-      },
-    }).then((res) => res.json().then((result) => setDadJoke(result)));
+  const fetchJoke = async () => {
+    const url = 'https://icanhazdadjoke.com/';
+
+    try {
+      let res = await fetch(url, {
+        headers: { Accept: 'application/json' },
+      });
+      const json = await res.json();
+      const joke = json.joke;
+      setDadJoke(joke);
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   useEffect(() => {
-    fetchJoke();
+    setDadJoke(fetchJoke());
   }, []);
-
-  const changeJoke = () => {
-    fetchJoke();
-    console.log(dadJoke);
-  };
 
   return (
     <Box>
@@ -44,10 +46,10 @@ export default function Home() {
         <HStack>
           <Button onClick={handleJoke}>Show Dad Joke</Button>
           <Button>
-            <RepeatIcon onClick={() => changeJoke()} />
+            <RepeatIcon onClick={fetchJoke} />
           </Button>
         </HStack>
-        {showJoke && <Text>{dadJoke.joke}</Text>}
+        {showJoke && <Text>{dadJoke}</Text>}
       </VStack>
     </Box>
   );
